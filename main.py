47dad2e6cafd74d81cd3a3afac83a01824c60da0
Ravin-Oyugi <ravinpeter01@gmail.com>
2022-11-15 06:40:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def word_replacer():
+
+    str = "Hello everybody i am avin and yes yes yes"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+
+word_replacer()
